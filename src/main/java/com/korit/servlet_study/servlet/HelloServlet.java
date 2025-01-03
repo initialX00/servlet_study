0490640fe6f2be112ac75ad7a3faebfc8ac968f7
initialX00 <rkdlswlsdn@naver.com>
2025-01-03 17:54:11 +0900

    1월 3일

---
+++ src/main/java/com/korit/servlet_study/servlet/HelloServlet.java
@@ -0,0 +1,30 @@
+package com.korit.servlet_study.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/helloservlet")  //주소설정
+public class HelloServlet extends HttpServlet {
+
+    String name = "aaa";
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println(req.getParameter("name"));
+
+        resp.getWriter().println("<html>" +
+                "<head>" +
+                "<title>Hello</title>" +
+                "</head>" +
+                "<body>" +
+                "<h1>Hello Servlet" + name + "</h1>" +
+                "</body>" +
+                "</html>");
+
+
+        req.getRequestDispatcher("/WEB-INF/hello.jsp").forward(req, resp);
+    }
+}
---
+++ src/main/webapp/WEB-INF/hello.jsp
@@ -0,0 +1,37 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: user
+  Date: 2025-01-03
+  Time: 오후 5:05
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%--해당태그가 html코드를 setvlet으로 변환시켜준다--%>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <% %> <!-- 스크립틀릿 : html에 자바코드를 적어 넣을 수 있는 태그 -->
+    <%
+        //String name = "bbb";
+        //int age = 32;
+    %>
+
+    <%
+        String name = request.getParameter("name");
+        int age = Integer.parseInt(request.getParameter("age")); //스트링으로만 값을 얻기에 형변환시켜준다
+        //입력된 값이 없기 때문에 값을 입력해준다
+        //html 주소에서 ? key=value를 입력할 수 있다. &로 여러개를 입력할 수 있다
+    %>
+
+    <h1>Hello Servlet</h1>
+    <h2>이름: <%=name %></h2> <!-- 스크립트릿에 =를 통해 변수를 불러올 수 있다-->
+    <h2>나이: <%=age %></h2>
+<!--f12로 html 소스에서 jsp파일 다운이 가능하여 보안에 취약하다
+    외부에서 접근이 불가한 WEB-INF를 활용하여 접근한다.
+  -->
+
+
+</body>
+</html>
